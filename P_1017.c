4b69d581883597629b341ea352c5ecf0688785b0
HenriqueFRX <132758752+HenriqueFRX@users.noreply.github.com>
2023-05-08 11:18:10 -0300

    Create P_1017.c

---
+++ P_1017.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int distancia, tempo;
+
+  printf("Digite a distancia: ");
+  scanf("%d", &distancia);
+
+  tempo = distancia * 2;
+
+    printf("%d minutos\n", tempo);
+ 
+ return 0;
+}
