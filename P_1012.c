ee4971a1e8e6242ee21fc5be84798fdace4035fd
HenriqueFRX <132758752+HenriqueFRX@users.noreply.github.com>
2023-05-22 22:50:09 -0300

    P_1012.c

--- P_1012.c
+++ P_1012.c
@@ -3,14 +3,14 @@
 
 int main() {
     double a, b, c;
-   printf("Digite os valores: ");
+ 
     scanf("%lf %lf %lf", &a, &b, &c);
 
 
     double area_triangulo = (a * c) / 2;
 
 
-    double area_circulo = M_PI * pow(c, 2);
+    double area_circulo = 3.14159 * pow(c, 2);
 
     double area_trapezio = ((a + b) * c) / 2;
 
@@ -18,11 +18,11 @@ int main() {
 
     double area_retangulo = a * b;
 
-    printf("TRIANGULO: %.2lf\n", area_triangulo);
-    printf("CIRCULO: %.2lf\n", area_circulo);
-    printf("TRAPEZIO: %.2lf\n", area_trapezio);
-    printf("QUADRADO: %.2lf\n", area_quadrado);
-    printf("RETANGULO: %.2lf\n", area_retangulo);
+    printf("TRIANGULO: %.3lf\n", area_triangulo);
+    printf("CIRCULO: %.3lf\n", area_circulo);
+    printf("TRAPEZIO: %.3lf\n", area_trapezio);
+    printf("QUADRADO: %.3lf\n", area_quadrado);
+    printf("RETANGULO: %.3lf\n", area_retangulo);
 
     return 0;
 }
